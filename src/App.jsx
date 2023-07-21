faf204fd1d8d44d9b3671fcede62d5948c7f80e6
dinethpiyumantha <dinethpiyumantha@gmail.com>
2023-07-21 23:34:46 +0530

    success ui impl.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Login from "./components/feature-auth/login/Login";
 import Register from "./components/feature-auth/register/Register";
 import EditProfile from "./components/feature-userprofile/edit-profile/EditProfile";
 import ViewProfile from "./components/feature-userprofile/view-profile/ViewProfile";
+import Success from "./components/feature-auth/success/Success";
 
 function App() {
   return (
-    <ViewProfile />
+    <Success />
   );
 }
 
