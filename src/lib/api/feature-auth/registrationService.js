c6d72a7262c7a4ed2bed07c28e608d286608077a
dinethpiyumantha <dinethpiyumantha@gmail.com>
2023-07-23 10:02:07 +0530

    commented - api services

--- src/lib/api/feature-auth/registrationService.js
+++ src/lib/api/feature-auth/registrationService.js
@@ -1,33 +1,43 @@
-// apiService.js
-// This file will handle the API requests
-
 import axios from "axios";
 
+// constants
 const BaseURL = "https://mditest.elifeamerica.com";
 const ClientId = 4;
 const ClientSecret = "sfQfhp6tDeGvEp7ZhVwk0MjbpaP55tJ1oJAKuZAE";
 const Scope = "";
 const GrantType = "password";
 
+/**
+ * Registration service
+ */
 const registrationService = {
+  /**
+   * Register a user
+   * @param {Object} user - user object from form (6 fileds sholud be required)
+   * @returns
+   */
   register: async (user) => {
     try {
+      // create formdata object using user object
       const userModified = new FormData();
+      userModified.append("email", user.email);
+      userModified.append("first_name", user.first_name);
+      userModified.append("last_name", user.last_name);
+      userModified.append("password", user.password);
+      userModified.append("confirm_password", user.confirm_password);
+      userModified.append("dob", `1994-01-16`);
+      userModified.append("mobile_number", `0${user.local_number}`);
+      userModified.append("gender", "male");
 
-      userModified.append('email', user.email);
-      userModified.append('first_name', user.first_name);
-      userModified.append('last_name', user.last_name);
-      userModified.append('password', user.password);
-      userModified.append('confirm_password', user.confirm_password);
-      userModified.append('dob', `1994-01-16`);
-      userModified.append('mobile_number', `0${user.local_number}`);
-      userModified.append('gender', "male");
-
-      const response = await axios.post(`https://mditest.elifeamerica.com/api/v1/register`, userModified, {
-        headers: {
-          'Content-Type': 'multipart/form-data', 
-        },
-      });
+      const response = await axios.post(
+        `https://mditest.elifeamerica.com/api/v1/register`,
+        userModified,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
       console.log(response);
       return response.data;
     } catch (error) {
