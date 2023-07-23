3bb1efd364446c6614fce720e48c0c5e7ba22f5f
dinethpiyumantha <dinethpiyumantha@gmail.com>
2023-07-23 10:09:08 +0530

    commented - all comps.

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,31 @@ import Register from "./components/feature-auth/register/Register";
 import EditProfile from "./components/feature-userprofile/edit-profile/EditProfile";
 import ViewProfile from "./components/feature-userprofile/view-profile/ViewProfile";
 import Success from "./components/feature-auth/success/Success";
-import { Navigate, Outlet, Route, Router, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+/**
+ * Privet route component
+ * @param {boolean} auth.isAuthenticated - if true only access this child component
+ * @param {Element} children - child component
+ * @returns
+ */
 const PrivateRoute = ({ auth: { isAuthenticated }, children }) => {
   return isAuthenticated ? children : <Navigate to="/auth" />;
 };
 
+/**
+ * App component
+ * @returns
+ */
 function App() {
+  // access token
+  const token = useSelector((state) => state.auth.auth.token);
 
-  const token = useSelector(state => state.auth.auth.token);
-
+  // check authenticated or not
   const isAuthenticated = () => {
-    return (token) ? true: false;
-  }
+    return token ? true : false;
+  };
 
   return (
     <Routes>
@@ -26,8 +37,24 @@ function App() {
       <Route exact path="/success" Component={Success} />
 
       {/* Private routes */}
-      <Route exact path="/" element={<PrivateRoute auth={{ isAuthenticated: isAuthenticated() }}><ViewProfile /></PrivateRoute>} />
-      <Route exact path="/profile-edit" element={<PrivateRoute auth={{ isAuthenticated: isAuthenticated() }}><EditProfile /></PrivateRoute>} />
+      <Route
+        exact
+        path="/"
+        element={
+          <PrivateRoute auth={{ isAuthenticated: isAuthenticated() }}>
+            <ViewProfile />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        exact
+        path="/profile-edit"
+        element={
+          <PrivateRoute auth={{ isAuthenticated: isAuthenticated() }}>
+            <EditProfile />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
-// store.js
+/**
+ * Store
+ */
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from "../ducks/feature-auth/reducer"
 
